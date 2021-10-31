60d51d3dd7e96e3a9b8bb70ca5adbf3138b5ebf8
Gorck1416 <78570619+Gorck1416@users.noreply.github.com>
2021-10-31 02:08:27 -0500

    Add files via upload

--- player.java
+++ player.java
@@ -1,11 +1,24 @@
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class player extends object{
   
-  public player(int x, int y, Name name){
+  private update upd;
+  private game Game;
+  
+  private BufferedImage img = null;
+    
+  public player(int x, int y, Name name, update upd, game Game){
     super(x, y, name);
+    this.upd = upd;
+    this.Game = Game;
 
   }
   
@@ -13,14 +26,39 @@ public class player extends object{
     x += velX;
     
     x = game.limit(x, game.leftSide, game.rightSide);
+    
+    collision();
   }
   
   public Rectangle getBounds(){
     return new Rectangle(x,y,32,32);
   }
-  public void render(Graphics g){
-    g.setColor(Color.cyan);
-    g.fillRect(x,y, 32, 32);
+  
+  private void collision(){
+    for(int i = 0; i < upd.obj.size(); i++){
+      object tempObj = upd.obj.get(i);
+      
+      if(tempObj.getName() == Name.wood) {
+          if(getBounds().intersects(tempObj.getBounds())) {
+            game.hit = true;
+            game.health--;
+            System.out.println(game.health);
+          }
+      }
+    }
   }
-}
+  
+  public void render(Graphics g){
+    try{
+      img = ImageIO.read(new File("person right.png"));
+    } catch (IOException e){
+      e.printStackTrace();
+    }
+    
+    
+    g.drawImage(img, x, y, 64, 64, Game);
+
+   }
+    
+ }
 
